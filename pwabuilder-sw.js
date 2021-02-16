a6ca3669df465e507a65e88dffdcc177d6232e32
ms-jahan <ssarwarjahan@gmail.com>
2021-02-16 21:45:48 +0600

    Added iplocation and timezone-based prayer time + some colors (worst at designing) + version change

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "pt-pwa-v0.0.4";
+const CACHE = "pt-pwa-v0.0.5";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
